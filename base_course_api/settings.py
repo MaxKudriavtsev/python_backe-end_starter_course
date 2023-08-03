ce66b7554dce47278572eb6a5b0d27abede0542e
Max Kudriavtsev <m0505179603@gmail.com>
2023-08-03 17:12:09 +0300

    added hello API View

--- base_course_api/settings.py
+++ base_course_api/settings.py
@@ -11,9 +11,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import sys
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -39,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'apps.profiles'
+    'profiles'
 ]
 
 MIDDLEWARE = [
