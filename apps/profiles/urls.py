714fa4d47ed0d9af33257d1055885325e30880b1
Max Kudriavtsev <m0505179603@gmail.com>
2023-08-04 11:03:33 +0300

    addded directory that is required for deployment, also some profile features

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path, include
-from . import views
 from rest_framework.routers import DefaultRouter
 from . import views
 
@@ -8,6 +7,7 @@ router = DefaultRouter()
 router.register('profile', views.UserProfileViewSet)
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view(),),
     path('', include(router.urls))
 ]
 
--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -8,7 +8,8 @@ from rest_framework import status
 from apps.profiles import serializers
 from apps.profiles import models
 from rest_framework import filters
-
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 
 class HelloApiView(APIView):
@@ -32,7 +33,7 @@ class HelloApiView(APIView):
         if serializer.is_valid():
             name = serializer.validated_data.get('name')
             message = f'Hello {name}'
-            return Response({'message':message})
+            return Response({'message': message})
         else:
             return Response(
                 serializer.errors,
@@ -60,3 +61,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
