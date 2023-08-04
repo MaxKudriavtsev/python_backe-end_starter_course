4835ff49916b5323d3b7165b76258e5f9b85e382
Max Kudriavtsev <m0505179603@gmail.com>
2023-08-04 03:53:51 +0300

    Created profile user model, implemented search and permission features

--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from rest_framework.authentication import TokenAuthentication
+from . import permissions
 from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 from apps.profiles import serializers
+from apps.profiles import models
+from rest_framework import filters
+
 
 
 class HelloApiView(APIView):
@@ -44,3 +50,13 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Delete an object"""
         return Response({'method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating, creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
